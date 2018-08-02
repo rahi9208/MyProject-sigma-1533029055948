9fe5b850f9e4d2e85f430a7c24327c6042bc9656
rahi9208 <rahi@wmail.club>
2018-08-02 10:31:04 +0530

    com

--- MyProject/lambda.js
+++ MyProject/lambda.js
@@ -1,6 +1,6 @@
 let AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
-    console.log("My P 3 My Okay");
-    callback(null,'Successfully executed');
+    console.log("My P 5 My Okay");
+    callback(null,'Successfully executed 5');
 }
